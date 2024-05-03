fcb5157348a600268e280c9c291fde02a181d914
MZso122 <micsko.zsombor+github@gmail.com>
2024-05-03 09:02:13 +0200

    ez lehet nem tokeletes...

--- gui.py
+++ gui.py
@@ -4,8 +4,22 @@ from matplotlib import pyplot
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import meghivogato_main as main
 import threading
+import string
+
+
+cluster_num2:int = 6
+print_extra_info:bool = False
+abrak:bool = False
+show_inertia_KMeans:bool = False
+show_KMeans_pelda:bool = False
+obj_path:string = 'raw_features_1st_q'
+obj_path_for_red:string = 'tomoritett_pirosak'
+
 
 class Application(tk.Frame):
+
+    
+
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
@@ -15,7 +29,7 @@ class Application(tk.Frame):
     def create_widgets(self):
         self.hi_there = tk.Button(self)
         self.hi_there["text"] = "Run program\n(click me)"
-        self.hi_there["command"] = self.say_hi
+        self.hi_there["command"] = self.say_hi()
         self.hi_there.pack(side="top")
 
         self.quit = tk.Button(self, text="QUIT", fg="red",
@@ -34,9 +48,11 @@ class Application(tk.Frame):
         # canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
 
     # def say_hi(self):
-    def say_hi(self):
-        thread = threading.Thread(target=main.main, args=(self,))
-        thread.start()
+    def say_hi(self):   # self, cluster_num2:int = 6, print_extra_info:bool = False, abrak:bool = False, show_inertia_KMeans:bool = True,
+                        # show_KMeans_pelda:bool = False, obj_path:string = 'raw_features_1st_q', obj_path_for_red:string = 'tomoritett_pirosak'):
+        thread = threading.Thread(target=main.main, args=(self, cluster_num2 , print_extra_info , abrak ,show_inertia_KMeans,
+                                  show_KMeans_pelda, obj_path, obj_path_for_red))
+        thread.start()  
 
         
 
