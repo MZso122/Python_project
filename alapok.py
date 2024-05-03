ee2f952c22bc34d4dbad905009e0a8cb53f80807
MZso122 <micsko.zsombor+github@gmail.com>
2024-05-03 08:14:40 +0200

    mult het vege actually

--- alapok.py
+++ alapok.py
@@ -71,7 +71,7 @@ def generateX_axis(data_):
 
 
 def main_fn(gui, cluster_num2:int = 6, print_extra_info:bool = False, abrak:bool = False, show_inertia_KMeans:bool = True, show_KMeans_pelda:bool = False,
-            obj_path = 'raw_features_1st_q', obj_path_for_red = 'tomoritett_pirosak'):
+            obj_path:string = 'raw_features_1st_q', obj_path_for_red:string = 'tomoritett_pirosak'):
 
 # if __name__ == '__main__':
     print_hi('PyCharm')
--- meghivogato_main.py
+++ meghivogato_main.py
@@ -2,8 +2,11 @@
 
 import alapok as alapok
 
-def main(gui):
-    alapok.main_fn(gui)
+
+def main(gui, cluster_num2:int = 6, print_extra_info:bool = False, abrak:bool = False, show_inertia_KMeans:bool = True,
+         show_KMeans_pelda:bool = False, obj_path:string = 'raw_features_1st_q', obj_path_for_red:string = 'tomoritett_pirosak'):
+    alapok.main_fn(gui, cluster_num2 = cluster_num2, print_extra_info = print_extra_info, abrak = abrak, show_inertia_KMeans = show_inertia_KMeans,
+                    show_KMeans_pelda = show_KMeans_pelda, obj_path = obj_path, obj_path_for_red = obj_path_for_red)
 
 
 
